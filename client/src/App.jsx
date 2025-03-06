155c94d80db46a84f7bb7377aca121aff9d78cc9
sarang <saranggp2018@gmail.com>
2025-03-06 23:50:54 +0530

    implemented protect routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,79 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import AuthLogin from './pages/auth/Login'
-import AuthLayout from './components/ui/auth/layout'
-import AuthRegister from './pages/auth/Register'
-import AdminLayout from './components/ui/admin-view/layout'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import AuthLogin from "./pages/auth/Login";
+import AuthLayout from "./components/auth/layout";
+import AuthRegister from "./pages/auth/Register";
+import AdminLayout from "./components/admin-view/layout";
+import AdminDashboard from "./pages/admin-view/dashboard";
+import AdminProducts from "./pages/admin-view/products";
+import AdminOrders from "./pages/admin-view/orders";
+import AdminFeatures from "./pages/admin-view/features";
+import ShoppingLayout from "./components/shopping-view/layout";
+import ShoppingAccount from "./pages/shopping-view/account";
+import ShoppingCheckout from "./pages/shopping-view/checkout";
+import ShoppingHome from "./pages/shopping-view/home";
+import ShoppingListing from "./pages/shopping-view/listing";
+import NotFound from "./pages/not-found/notfound";
+import CheckAuth from "./components/common/check-auth";
 
 function App() {
   return (
     <div>
-      App     
-
+      App
       <Routes>
-      <Route path ='/auth' element = {<AuthLayout/>}>
-        <Route path='login' element = {<AuthLogin/>}/>
-        <Route path='register' element = {<AuthRegister/>} />
-      </Route>
+        <Route
+          path="/auth"
+          element={
+            <CheckAuth>
+              <AuthLayout />
+            </CheckAuth>
+          }
+        >
+          <Route path="login" element={<AuthLogin />} />
+          <Route path="register" element={<AuthRegister />} />
+        </Route>
 
-      <Route>
-        <Route path='/admin' element ={<AdminLayout/>}/>
-        
-      </Route>
+        <Route>
+          <Route
+            path="/admin"
+            element={
+              <CheckAuth>
+                <AdminLayout />
+              </CheckAuth>
+            }
+          >
+            <Route path="dashboard" element={<AdminDashboard />} />
+            <Route path="products" element={<AdminProducts />} />
+            <Route path="orders" element={<AdminOrders />} />
+            <Route path="features" element={<AdminFeatures />} />
+          </Route>
+        </Route>
+
+        <Route
+          path="/shop"
+          element={
+            <CheckAuth>
+              <ShoppingLayout />
+            </CheckAuth>
+          }
+        >
+          <Route path="account" element={<ShoppingAccount />} />
+          <Route path="checkout" element={<ShoppingCheckout />} />
+          <Route path="home" element={<ShoppingHome />} />
+          <Route path="listing" element={<ShoppingListing />} />
+        </Route>
+
+        <Route
+          path="*"
+          element={
+            <CheckAuth>
+              <NotFound />
+            </CheckAuth>
+          }
+        />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
