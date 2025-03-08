cb38225d03f9b5827512996a5d6826b7062cb4d8
sarang <saranggp2018@gmail.com>
2025-03-08 20:55:21 +0530

    completed cart slicing

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,7 +2,9 @@ import  { configureStore } from "@reduxjs/toolkit"
 import authReducer from './auth-slice/index';
 import adminProductSlice from './admin/product-slice/index';
 
-import shopProductsSlice from "../store/shop/product-slice";
+import shopProductsSlice from "../store/shop/product-slice/index";
+import shopCartSlice from '../store/shop/cart-slice/index'
+
 
 const store = configureStore({
     reducer : {
@@ -10,6 +12,7 @@ const store = configureStore({
         adminProducts : adminProductSlice,
 
         shopProducts : shopProductsSlice,
+        shopCart : shopCartSlice
     }
 })
 
