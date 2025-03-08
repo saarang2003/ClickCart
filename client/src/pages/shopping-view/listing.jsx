2b05b4f5546155a57dfb03e067109caf74f4fefa
sarang <saranggp2018@gmail.com>
2025-03-08 08:36:45 +0530

    implemented filter functionality

--- client/src/pages/shopping-view/listing.jsx
+++ client/src/pages/shopping-view/listing.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import ProductFilter from '../../components/shopping-view/filter'
 
 function ShoppingListing() {
+
+  const [filter , setFilter] = useState({});
+
+function handleFilter() {
+  
+}
   return (
-    <div>ShoppingListing</div>
+    <div className="grid grid-cols-1 md:grid-cols-[200px_1fr] gap-6 p-4 md:p-6">
+         <ProductFilter filters={filter} handleFilter={handleFilter} />
+
+
+    </div>
   )
 }
 
