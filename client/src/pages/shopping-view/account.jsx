31495a57b477db4ac085880a352e3fa84d5dc329
sarang <saranggp2018@gmail.com>
2025-03-09 17:12:31 +0530

    working on acoutn functionalkity

--- client/src/pages/shopping-view/account.jsx
+++ client/src/pages/shopping-view/account.jsx
@@ -3,6 +3,20 @@ import React from 'react'
 function ShoppingAccount() {
   return (
     <div className="flex flex-col">
+     <div className="relative h-[300px] w-full overflow-hidden">
+     <img
+          src={accImg}
+          className="h-full w-full object-cover object-center"
+        />
+      </div>
+      <div className="container mx-auto grid grid-cols-1 gap-8 py-8">
+      <div className="flex flex-col rounded-lg border bg-background p-6 shadow-sm">
+        
+        
+        </div>
+        
+        
+        </div>
       
     </div>
   )
