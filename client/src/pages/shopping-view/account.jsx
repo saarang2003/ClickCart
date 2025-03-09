c39c0b1a47bc1cff9662ff6bba3bbfe2ab5b6a42
sarang <saranggp2018@gmail.com>
2025-03-09 17:38:15 +0530

    completed order component

--- client/src/pages/shopping-view/account.jsx
+++ client/src/pages/shopping-view/account.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import accImg from '../../assets/account.jpg';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '../../components/ui/tabs'
 
 function ShoppingAccount() {
   return (
@@ -11,13 +13,20 @@ function ShoppingAccount() {
       </div>
       <div className="container mx-auto grid grid-cols-1 gap-8 py-8">
       <div className="flex flex-col rounded-lg border bg-background p-6 shadow-sm">
-        
-        
+      <Tabs defaultValue="orders">
+            <TabsList>
+              <TabsTrigger value="orders">Orders</TabsTrigger>
+              <TabsTrigger value="address">Address</TabsTrigger>
+            </TabsList>
+            <TabsContent value="orders">
+              <ShoppingOrders />
+            </TabsContent>
+            <TabsContent value="address">
+              <Address />
+            </TabsContent>
+          </Tabs>
+        </div>    
         </div>
-        
-        
-        </div>
-      
     </div>
   )
 }
