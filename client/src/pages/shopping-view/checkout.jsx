6c6b764cca2d68b649cdc2bb210d74cc248a3a70
sarang <saranggp2018@gmail.com>
2025-03-09 18:21:21 +0530

    completed checkout page frontend

--- client/src/pages/shopping-view/checkout.jsx
+++ client/src/pages/shopping-view/checkout.jsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import React from "react";
 
 function ShoppingCheckout() {
   return (
-    <div>ShoppingCheckout</div>
-  )
+    <div className="flex flex-col">
+      <div className="relative h-[300px] w-full overflow-hidden">
+        <img src={img} className="h-full w-full object-cover object-center" />
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 mt-5 p-5">
+        <Address
+          selectedId={currentSelectedAddress}
+          setCurrentSelectedAddress={setCurrentSelectedAddress}
+        />
+        <div className="flex flex-col gap-4">
+          {cartItems && cartItems.items && cartItems.items.length > 0
+            ? cartItems.items.map((item) => (
+                <UserCartItemsContent cartItem={item} />
+              ))
+            : null}
+
+          <div className="mt-8 space-y-4">
+            <div className="flex justify-between">
+              <span className="font-bold">Total</span>
+              <span className="font-bold">${totalCartAmount}</span>
+            </div>
+          </div>
+
+          <div className="mt-4 w-full">
+            <Button onClick={handleInitiatePaypalPayment} className="w-full">
+              {isPaymentStart
+                ? "Processing Paypal Payment..."
+                : "Checkout with Paypal"}
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ShoppingCheckout
+export default ShoppingCheckout;
