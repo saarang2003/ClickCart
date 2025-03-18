3dad28f7cc5db9a17369d6dfeef7a3ed54d31576
sarang <saranggp2018@gmail.com>
2025-03-18 11:09:24 +0530

    wrote admin side order business logic

--- server/controllers/admin/order-controller.js
+++ server/controllers/admin/order-controller.js
@@ -1,216 +1,74 @@
-
-const paypal = require('../../helpers/paypal');
 const Order = require('../../models/Order');
-const Cart = require('../../models/Cart');
-const Product = require('../../models/Product');
 
 
+const getAllOrdersOfAllUsers = async( req,res) =>{
+  try {
 
 
-const createOrder = async (req, res) => {
-    try {
-      const {
-        userId,
-        cartItems,
-        addressInfo,
-        orderStatus,
-        paymentMethod,
-        paymentStatus,
-        totalAmount,
-        orderDate,
-        orderUpdateDate,
-        paymentId,
-        payerId,
-        cartId,
-      } = req.body;
-  
-      const create_payment_json = {
-        intent: "sale",
-        payer: {
-          payment_method: "paypal",
-        },
-        redirect_urls: {
-          return_url: "http://localhost:5173/shop/paypal-return",
-          cancel_url: "http://localhost:5173/shop/paypal-cancel",
-        },
-        transactions: [
-          {
-            item_list: {
-              items: cartItems.map((item) => ({
-                name: item.title,
-                sku: item.productId,
-                price: item.price.toFixed(2),
-                currency: "USD",
-                quantity: item.quantity,
-              })),
-            },
-            amount: {
-              currency: "USD",
-              total: totalAmount.toFixed(2),
-            },
-            description: "description",
-          },
-        ],
-      };
-  
-      paypal.payment.create(create_payment_json, async (error, paymentInfo) => {
-        if (error) {
-          console.log(error);
-  
-          return res.status(500).json({
-            success: false,
-            message: "Error while creating paypal payment",
-          });
-        } else {
-          const newlyCreatedOrder = new Order({
-            userId,
-            cartId,
-            cartItems,
-            addressInfo,
-            orderStatus,
-            paymentMethod,
-            paymentStatus,
-            totalAmount,
-            orderDate,
-            orderUpdateDate,
-            paymentId,
-            payerId,
-          });
-  
-          await newlyCreatedOrder.save();
-  
-          const approvalURL = paymentInfo.links.find(
-            (link) => link.rel === "approval_url"
-          ).href;
-  
-          res.status(201).json({
-            success: true,
-            approvalURL,
-            orderId: newlyCreatedOrder._id,
-          });
-        }
-      });
-    } catch (e) {
-      console.log(e);
-      res.status(500).json({
-        success: false,
-        message: "Some error occured!",
-      });
-    }
-  };
-  
+    const orders = await Order.find({});
 
-
-  const capturePayment = async(req,res) =>{
-try {
-      
-    const {paymentId , payerId , orderId} = req.body;
-
-    let order = await Order.findById(orderId);
-
-    if(!order){
+    if(!orders.length){
       return res.status(404).json({
-        success : false,
-        message : "Order can not be found"
-      })
+        success: false,
+        message: "No orders found!",
+      });
     }
 
-    order.paymentStatus = 'paid';
-    order.orderStatus = 'confirmed';
-    order.paymentId = paymentId;
-    order.payerId = payerId;
-
-    const getCartId = order.cartId;
-    await Cart.findByIdAndDelete(getCartId);
-
-
-
-    await order.save();
 
     res.status(200).json({
-      success :true,
-      message : 'Order Confirmed',
-      data : order
-    }
-    )
-} catch (e) {
-  console.log(e);
-  res.status(500).json({
-    success: false,
-    message: "Some error occured!",
-  });
-
+      success: true,
+      data: orders,
+    });
+    
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured!",
+    });
+  }
 }
-  }
 
 
-  const getAllOrderByUser = async(req , res) =>{
 
-    try {
-      
-      const {userId} = req.params;
+const getOrderDetailsForAdmin = async(req,res) =>{
+  try {
+    
 
-      const orders = await Order.find({userId});
+    const {id}  = req.params;
+    const order = await Order.findById(id);
 
-      if(!orders.length){
-        return res.status(404).json({
-          success:false,
-          message : "No orders found!",
-        })
-      }
-
-
-      res.status(200).json({
-        success:true,
-        data :orders
-      })
-
-    } catch (e) {
-      console.log(e);
-  res.status(500).json({
-    success: false,
-    message: "Some error occured!",
-  });
+    
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        message: "Order not found!",
+      });
     }
+    res.status(200).json({
+      success: true,
+      data: order,
+    });
+
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured!",
+    });
   }
+}
 
 
-  const getOrderDetails = async(req,res) =>{
-    try {
-
-        const {id} = req.params ;
-      
-        const order = await Order.findById({id});
-
-        if(!order.length){
-          return res.status(404).json({
-            success: false,
-            message : "No such order"
-          })
-        }
-
-
-            res.status(200).json({
-              success: true,
-              data : order
-            })
-
-
-      
-    } catch (e) {
-      console.log(e);
-  res.status(500).json({
-    success: false,
-    message: "Some error occured!",
-  });
-    }
+const updateOrderStatus = async(req,res) =>{
+  try {
+    
+    
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Some error occured!",
+    });
   }
-
-
-
-  module.exports = {
-    createOrder,
-    capturePayment,
-    getAllOrderByUser,
-    getOrderDetails
-  }
+}
