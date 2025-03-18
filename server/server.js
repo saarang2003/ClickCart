e58b641e96b5bc6d557e5075741b4afeefa4fe0f
sarang <saranggp2018@gmail.com>
2025-03-18 19:13:25 +0530

    completed routing part

--- server/server.js
+++ server/server.js
@@ -5,11 +5,15 @@ const connectDb = require('./utils/db.js');
 const cookieParser = require('cookie-parser');
 const authRouter = require('./routes/auth/auth-routes.js');
 const adminProductsRouter = require('./routes/admin/product-routes.js');
+const adminOrderRouter = require('./routes/admin/order-routes.js');
+
+
 const shopProductsRouter = require('./routes/shop/products-routes.js');
 const  shopCartRouter = require('./routes/shop/cart-routes.js');
 const shopAddressRouter = require('./routes/shop/address-routes.js');
 const shopOrderRouter = require('./routes/shop/order-routes.js');
-
+const shopSearchRouter =- require('./routes/shop/search-routes.js');
+const shopReviewRouter = require('./routes/shop/review-route.js');
 
 connectDb();
 const app = express()
@@ -36,9 +40,14 @@ app.use(
 
   app.use('/api/auth' , authRouter);
   app.use('/api/admin/products' , adminProductsRouter)
+  app.use('/api/auth/orders' , adminOrderRouter);
 
   app.use('/api/shop/products' , shopProductsRouter)
   app.use("/api/shop/cart", shopCartRouter);
   app.use('/api/shop/address' ,shopAddressRouter )
   app.use('/api/shop/order' , shopOrderRouter);
+  app.use('/api/shop/search' , shopSearchRouter);
+  app.use('/api/shop/review' , shopReviewRouter)
+
+  
   app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
