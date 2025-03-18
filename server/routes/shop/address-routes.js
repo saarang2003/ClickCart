70d6b2a3ed3bcb47a1ac10ffbfdcd2868c65870c
sarang <saranggp2018@gmail.com>
2025-03-18 19:00:21 +0530

    fixed shop side address routing

--- server/routes/shop/address-routes.js
+++ server/routes/shop/address-routes.js
@@ -6,12 +6,12 @@ const express = require('express');
 const router = express.Router();
 
 
-const { addAddress, fetchAllAddress, editAllAddress, deleteAddress }  = require('../../controllers/shop/address-controller');
+const { addAddress, fetchAllAddress, deleteAddress, editAddress }  = require('../../controllers/shop/address-controller');
 
 
 router.post('/add' , addAddress);
 router.get('/get/:userId' , fetchAllAddress);
-router.put('/update/:userId/:addressId' , editAllAddress);
-router.delete('/delete/:userId/:addressId' , deleteAddress);
+router.delete("/delete/:userId/:addressId", deleteAddress);
+router.put("/update/:userId/:addressId", editAddress);
 
 module.exports = router;
