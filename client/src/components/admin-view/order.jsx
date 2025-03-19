a2965a993e43884b731b619d160f787fab8f659d
sarang <saranggp2018@gmail.com>
2025-03-19 18:23:25 +0530

    completed admin order view functionality

--- client/src/components/admin-view/order.jsx
+++ client/src/components/admin-view/order.jsx
@@ -1,69 +1,94 @@
-import React, { useState } from 'react'
-import { Card, CardContent, CardHeader, CardTitle } from '../ui/card'
-import { Table, TableHead, TableHeader, TableRow } from '../ui/table'
+import React, { useEffect, useState } from 'react';
+import { Card, CardContent, CardHeader, CardTitle } from '../ui/card';
+import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '../ui/table';
 import { useDispatch, useSelector } from 'react-redux';
-import {toast} from 'sonner';
-import { getAllOrdersForAdmin } from '../../store/admin/order-slice/index';
+import { toast } from 'sonner';
+import { getAllOrdersForAdmin, getOrderDetailsForAdmin, resetOrderDetails } from '../../store/admin/order-slice/index';
+import { Badge } from '../ui/badge';
+import { Button } from '../ui/button';
+import AdminOrderDetailsView from './order-details';
+import { Dialog } from '../ui/dialog';
 
-
-const initialFormData = {
-  status : "",
-}
-
-function AdminOrdersView({orderDetails}) {
+function AdminOrdersView() {
   const [openDetailsDialog, setOpenDetailsDialog] = useState(false);
-  const [formdata , setFormData] = useState(initialFormData);
-  const {user} = useSelector((state) => state.auth);
+  const [selectedOrderId, setSelectedOrderId] = useState(null); // Track which order's details to show
   const { orderList, orderDetails } = useSelector((state) => state.adminOrder);
-
   const dispatch = useDispatch();
 
-    console.log(orderDetails , "orderDetailsorder");
+  function handleFetchOrderDetails(orderId) {
+    dispatch(getOrderDetailsForAdmin(orderId));
+    setSelectedOrderId(orderId);
+    setOpenDetailsDialog(true); // Open dialog when details are fetched
+  }
 
-    function handleFetchOrderDetails(getId) {
-      dispatch(getAllOrdersForAdmin(getId));
-    }
-  
+  useEffect(() => {
+    dispatch(getAllOrdersForAdmin());
+  }, [dispatch]);
 
-    function handleUpdateStatus(event){
-      event.preventDefault();
-      const {status} = formdata;
-
-      dispatch(
-        updateOrderStatus({
-          id : orderDetails?._id , orderStatus : status
-        })
-      ).then((data) =>{
-        if(data?.payload?.success){
-          dispatch(getOrderDetailsForAdmin(orderDetails._id));
-          dispatch(getAllOrderForAdmin());
-          setFormData(initialFormData)
-        }
-      })
-    }
+  const handleCloseDialog = () => {
+    setOpenDetailsDialog(false);
+    dispatch(resetOrderDetails());
+  };
 
   return (
     <Card>
-         <CardHeader>
-           <CardTitle>All Orders</CardTitle>
-         </CardHeader>
-         <CardContent>
-           <Table>
-             <TableHeader>
-               <TableRow>
-                 <TableHead>Order Id</TableHead>
-                 <TableHead>Order Date</TableHead>
-                 <TableHead>Order Status</TableHead>
-                 <TableHead>Order Price</TableHead>
-                 <TableHead>
-                   <span className="sr-only">Details</span>
-                 </TableHead>
-               </TableRow>
-             </TableHeader>
-           </Table>
-         </CardContent>
-       </Card>
-  )
+      <CardHeader>
+        <CardTitle>All Orders</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Order Id</TableHead>
+              <TableHead>Order Date</TableHead>
+              <TableHead>Order Status</TableHead>
+              <TableHead>Order Price</TableHead>
+              <TableHead>
+                <span className="sr-only">Details</span>
+              </TableHead>
+            </TableRow>
+          </TableHeader>
+
+          <TableBody>
+            {orderList && orderList.length > 0
+              ? orderList.map((orderItem) => (
+                  <TableRow key={orderItem._id}>
+                    <TableCell>{orderItem._id}</TableCell>
+                    <TableCell>{orderItem.orderDate.split('T')[0]}</TableCell>
+                    <TableCell>
+                      <Badge
+                        className={`py-1 px-3 ${
+                          orderItem.orderStatus === 'confirmed'
+                            ? 'bg-green-500'
+                            : orderItem.orderStatus === 'rejected'
+                            ? 'bg-red-600'
+                            : 'bg-black'
+                        }`}
+                      >
+                        {orderItem.orderStatus}
+                      </Badge>
+                    </TableCell>
+                    <TableCell>${orderItem.totalAmount}</TableCell>
+                    <TableCell>
+                      <Button onClick={() => handleFetchOrderDetails(orderItem._id)}>
+                        View Details
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))
+              : null}
+          </TableBody>
+        </Table>
+      </CardContent>
+
+      {/* Dialog to show order details */}
+      <Dialog open={openDetailsDialog} onOpenChange={handleCloseDialog}>
+        {selectedOrderId && (
+          <AdminOrderDetailsView orderDetails={orderDetails} />
+        )}
+      </Dialog>
+    </Card>
+  );
 }
 
-export default AdminOrdersView
+export default AdminOrdersView;
