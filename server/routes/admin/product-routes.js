4d38c9dca111e0b3a4d2bba99ea3cea76283633a
sarang <saranggp2018@gmail.com>
2025-06-14 08:33:34 +0530

    corrected edit routing error

--- server/routes/admin/product-routes.js
+++ server/routes/admin/product-routes.js
@@ -8,7 +8,7 @@ const router = express .Router();
 
 router.post('/upload-image' , upload.single("my_file") , handleImageUpload );
 router.post('/add' , addProduct);
-router.put('/edit' , editProduct);
+router.put('/edit/:id' , editProduct);
 router.delete('/delete/:id' , deleteProduct);
 router.get('/get' , fetchAllProducts);
 
